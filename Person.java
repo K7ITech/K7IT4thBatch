3d6d6e4c2ce741a90f48b687ab3f88c0741275c1
Kalaiselvi <kalai.cseanna@gmail.com>
2023-02-16 20:05:45 +0530

    add log4j

---
+++ Person.java
@@ -0,0 +1,15 @@
+import org.apache.log4j.Logger;
+
+public class Person {
+	private static Logger log = Logger.getLogger(Person.class);
+
+	public static void main(String[] args) {
+	  
+		
+		  log.debug("A");
+		  log.info(2);
+		  log.error(6);
+		  log.fatal("B");
+	}
+
+}
