a56473bf9d7ded26d91ee3a6cd63b2645d9f0837
Latha <Lathagurram5@gmail.com>
2023-02-17 15:51:12 +0530

    added Monkey.java

---
+++ Monkey.java
@@ -0,0 +1,13 @@
+import org.apache.log4j.Logger;
+
+public class Monkey {
+private static Logger log=Logger.getLogger(Monkey.class);
+	public static void main(String[] args) {
+log.debug("abc");
+log.equals("xyz");
+log.fatal("pqr");
+log.info("ijk");
+
+	}
+
+}
