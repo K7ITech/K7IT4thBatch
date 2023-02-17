d7e4a6a7435e1f86c2c13257e00b33d0a967c7c8
lovelycherry21 <mallelababu91@gmail.com>
2023-02-17 19:18:29 +0530

    test me sir this code VinnuHero.java

---
+++ VinnuHero.java
@@ -0,0 +1,18 @@
+import org.apache.log4j.Logger;
+
+public class VinnuHero {
+
+	private static Logger log = Logger.getLogger(VinnuHero.class);
+
+	public static void main(String[] args) {
+		
+		log.debug("mysir-my god");
+		log.info("medam");
+		log.error("lucky");
+		log.fatal("vinnu");
+		log.fatal("mysir-my god");
+		log.debug("mysir-my god");
+		log.debug("mysir-my god");
+	}
+
+}
