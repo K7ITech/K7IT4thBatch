dee8abc1f62700fe0cbb6ad3dfb941861fb2e370
Latha <Lathagurram5@gmail.com>
2023-02-17 14:38:42 +0530

    G.java

---
+++ G.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+
+public class G 
+{
+	private static Logger log = Logger.getLogger(G.class);
+
+	public static void main(String[] args) {
+		log.debug("Latha");
+		log.error("aruna");
+		log.fatal("Test");
+		log.info("sathish");
+	}
+
+}
