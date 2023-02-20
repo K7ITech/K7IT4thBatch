23bed17f69acc60d7abb3ebfdf5bd2f9a783770e
uplift-technoelevate <manjunathachannappagowdru@gmail.com>
2023-02-20 18:39:01 +0530

    added Array.java

---
+++ Array.java
@@ -0,0 +1,38 @@
+package com.k7it;
+
+public class Array {
+
+	public static void main(String[] args)
+	{	
+  int a[][]=new int[4][4];
+  int value=1;
+  int size=4;
+  for (int i=0;i<size;i++)
+  {
+	  for(int j=0;j<size;j++)
+	  {
+		  a[i][j]=value;
+		  value++;
+	  }
+  }
+  for(int i=0;i<4;i++)
+  {
+    for(int j=0;j<4;j++)
+    {
+		  if(i==j)
+		  {
+			  System.out.print(a[i][j]+"\t");
+			  
+		  }
+		  else
+		  
+			  System.out.print("*"+"\t");
+			
+		  }
+		
+			  System.out.println("");
+	  }
+    
+  }
+}
+
