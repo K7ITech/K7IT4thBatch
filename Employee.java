8ed0371ea80d9b6809093b43dedcb6ba053769ed
lovelycherry21 <mallelababu91@gmail.com>
2023-02-20 17:50:39 +0530

    added Employee class

---
+++ Employee.java
@@ -0,0 +1,44 @@
+class employee
+{
+private int id;
+private string name;
+private string sal;
+private string dept;
+
+Public void set Id(Int id)
+ {
+this id=id;
+}
+Public int get Id()
+{
+ruturn id;
+}
+Public void set Name(String name)
+{
+this Name=neme;
+}
+Public string get Name()
+{
+ruturn Name;
+}
+Public void set Salary(Long salary) 
+{
+this Salary=salary;
+}
+Public Long get sal()
+{
+ruturn long;
+}
+Public void Dept(String dept) 
+{
+this Dept=department;
+}
+Public String Get dept();
+{
+return department;
+  }
+}
+
+
+
+
