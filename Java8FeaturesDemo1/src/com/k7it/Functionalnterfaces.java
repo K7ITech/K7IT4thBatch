4487102db18b795d2f7ca42de94014cb64bc8906
Latha <Lathagurram5@gmail.com>
2023-02-28 18:30:06 +0530

    Java8FeaturesDemo1/.project is added

---
+++ Java8FeaturesDemo1/src/com/k7it/Functionalnterfaces.java
@@ -0,0 +1,26 @@
+package com.k7it;
+
+import java.util.function.Function;
+
+public class Functionalnterfaces
+{
+	public static boolean isEvenNumber(int n) {
+		return n%2==0;
+	}
+	
+	public static void main(String[] args) {
+		
+		Function<Integer,Integer>squared=(n) -> n*n;
+		
+		System.out.println("square of 4:"+squared.apply(4));
+		System.out.println("square of 5:"+squared.apply(5));
+		
+		java.util.function.Predicate<Integer>Predicate=(n) -> n%2==0;
+		
+		System.out.println("4 is even number:"+Predicate.test(4));
+		System.out.println("5 is even number:"+Predicate.test(5));
+		
+
+	}
+
+}
