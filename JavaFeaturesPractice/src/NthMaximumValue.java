8deaa665daa99de337617230faac4e87b7de5d35
kiran <pkiranreddy93@gmail.com>
2023-03-07 12:08:08 +0530

    JavaFeaturesPractice/.java is added

---
+++ JavaFeaturesPractice/src/NthMaximumValue.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class NthMaximumValue {
+	private static final Logger log=Logger.getLogger(NthMaximumValue.class);
+public static void main(String[] args) {	
+	
+	}
+}
+
+
+	
+
+
+
