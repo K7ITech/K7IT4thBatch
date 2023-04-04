5c34d59636cc41afa4d0058f27ae25caeb2bdbba
Anuradha <anuradhajbiradar@gmail.com>
2023-04-04 12:02:04 +0530

    added array.java

--- Array.java
+++ Array.java
@@ -1,38 +1,35 @@
 package com.k7it;
 
-public class Array {
+public class Array{
 
-	public static void main(String[] args)
-	{	
-  int a[][]=new int[4][4];
-  int value=1;
-  int size=4;
-  for (int i=0;i<size;i++)
-  {
-	  for(int j=0;j<size;j++)
-	  {
-		  a[i][j]=value;
-		  value++;
-	  }
-  }
-  for(int i=0;i<4;i++)
-  {
-    for(int j=0;j<4;j++)
+
+static int GetLargestSum(int[] array, int n)
+{
+    int largestSum = 0;
+    int previousSum = 0;
+
+    for (int i = 0; i <= array.length - n; i++)
     {
-		  if(i==j)
-		  {
-			  System.out.print(a[i][j]+"\t");
-			  
-		  }
-		  else
-		  
-			  System.out.print("*"+"\t");
-			
-		  }
-		
-			  System.out.println("");
-	  }
-    
-  }
-}
+        if (i == 0)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                largestSum += array[j];
+            }
 
+            previousSum = largestSum;
+        }
+        else
+        {
+            int currentSum = previousSum - array[i - 1] + array[i + n - 1];
+            if (currentSum > largestSum)
+            {
+                largestSum = currentSum;
+            }
+            previousSum = currentSum;
+        }
+    }
+
+    return largestSum;
+}
+}
